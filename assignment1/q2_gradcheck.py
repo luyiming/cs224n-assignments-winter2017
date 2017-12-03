08ecd894cca291494719af0e6614080fec81953a
luyiming <luyimingchn@gmail.com>
2017-12-03 17:12:21 +0800

    q2_gradcheck

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -30,21 +30,29 @@ def gradcheck_naive(f, x):
         # to test cost functions with built in randomness later.
 
         ### YOUR CODE HERE:
-        raise NotImplementedError
+        oldval = x[ix]
+        x[ix] = oldval + h  # increment by h
+        random.setstate(rndstate)
+        fxph, _ = f(x)  # evalute f(x + h)
+        x[ix] = oldval - h
+        random.setstate(rndstate)
+        fxmh, _ = f(x)  # evaluate f(x - h)
+        x[ix] = oldval  # restore
+        # compute the partial derivative with centered formula
+        numgrad = (fxph - fxmh) / (2 * h)  # the slope
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (
-                grad[ix], numgrad)
+            print("Gradient check failed.")
+            print("First gradient error found at index %s" % str(ix))
+            print("Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad))
             return
 
         it.iternext() # Step to next dimension
 
-    print "Gradient check passed!"
+    print("Gradient check passed!")
 
 
 def sanity_check():
@@ -53,11 +61,11 @@ def sanity_check():
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print("Running sanity checks...")
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
     gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
+    print("")
 
 
 def your_sanity_checks():
@@ -67,9 +75,14 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    print("Running your sanity checks...")
     ### YOUR CODE HERE
-    raise NotImplementedError
+    func = lambda x: (np.sum(np.exp(x)), np.exp(x))
+    print("Running sanity checks...")
+    gradcheck_naive(func, np.array(123.456))      # scalar test
+    gradcheck_naive(func, np.random.randn(3,))    # 1-D test
+    gradcheck_naive(func, np.random.randn(4, 5))   # 2-D test
+    print("")
     ### END YOUR CODE
 
 
